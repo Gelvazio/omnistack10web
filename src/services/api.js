e56bcd33b7730ca6f223069a4c74e39ca91aedaa
Gelvazio Camargo <gelvazio@hotmail.com>
2020-02-02 16:02:47 -0200

    aaaaa

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import {getToken} from './auth'
 
 const api = axios.create({
-  baseURL: 'https://findevs-backend.herokuapp.com/',
+  //baseURL: 'https://findevs-backend.herokuapp.com/',
+  baseURL: 'https://omnistack10api.herokuapp.com/',
 });
 
 api.interceptors.request.use(async config => {
